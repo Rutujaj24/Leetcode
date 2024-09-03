d382c1751be3f00e76c5aabbb80368f9343c00b7
Rutuja Jadhav <133589775+Rutujaj24@users.noreply.github.com>
2024-09-03 22:40:19 +0530

    Time: 2 ms (14.84%), Space: 42.1 MB (9.33%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(nums[j-1] > nums[j]){  
+                                 //swap elements  
+                                 temp = nums[j-1];  
+                                 nums[j-1] = nums[j];  
+                                 nums[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+    }
+}
+      
+        
